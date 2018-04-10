8bd3ca9de2467972cbebadd3587fa68eae6de783
unknown <portillozachary@gmail.com>
2018-04-10 16:58:59 -0700

    finally added base files

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
@@ -12,36 +11,33 @@ import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			StartUpScreen start = new StartUpScreen();
-			MainMenu mainMenu = new MainMenu();
-			Settings settingMenu = new Settings();
-			
 
-			Scene mainScene = new Scene(start);
-			mainScene.getStylesheets().add(start.getClass().getResource("styling/StartUpScreen.css").toExternalForm());
-			mainScene.getStylesheets().add(mainMenu.getClass().getResource("styling/MainMenu.css").toExternalForm());
-			mainScene.getStylesheets().add(settingMenu.getClass().getResource("styling/Settings.css").toExternalForm());
-                        
-			
-			start.setOnMouseClicked(e -> mainScene.setRoot(mainMenu));
-			mainMenu.getSettingMenu().setOnAction(e -> mainScene.setRoot(settingMenu));
-			
-			primaryStage.setScene(mainScene);
-			primaryStage.setFullScreen(true);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-		
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            StartUpScreen start = new StartUpScreen();
+            MainMenu mainMenu = new MainMenu();
+            Settings settingMenu = new Settings();
+
+            Scene mainScene = new Scene(start);
+            mainScene.getStylesheets().add(start.getClass().getResource("styling/StartUpScreen.css").toExternalForm());
+            mainScene.getStylesheets().add(mainMenu.getClass().getResource("styling/MainMenu.css").toExternalForm());
+            mainScene.getStylesheets().add(settingMenu.getClass().getResource("styling/Settings.css").toExternalForm());
+
+            start.setOnMouseClicked(e -> mainScene.setRoot(mainMenu));
+            mainMenu.getSettingMenu().setOnAction(e -> mainScene.setRoot(settingMenu));
+
+            primaryStage.setScene(mainScene);
+            primaryStage.setFullScreen(true);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
 }
-
--- src/application/StartUpScreen.java
+++ src/application/StartUpScreen.java
@@ -6,17 +6,17 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 
-public class StartUpScreen extends StackPane{
-	public StartUpScreen() {
-		Image image = new Image("wallpapers/cerealWallpaper.jpg");
-		ImageView view = new ImageView(image);
-	    Text touchBegin = new Text("Touch to Begin");
-	    touchBegin.setId("touchToBegin");
-	    getChildren().addAll(view, touchBegin);
-	    
-	    setAlignment(Pos.CENTER);
-	    
-	    
-	}
-	
+public class StartUpScreen extends StackPane {
+
+    public StartUpScreen() {
+        Image image = new Image("resources/wallpapers/cerealWallpaper.jpg");
+        ImageView view = new ImageView(image);
+        Text touchBegin = new Text("Touch to Begin");
+        touchBegin.setId("touchToBegin");
+        getChildren().addAll(view, touchBegin);
+
+        setAlignment(Pos.CENTER);
+
+    }
+
 }
