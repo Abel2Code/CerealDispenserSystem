be528c77c2fe5eb0ff81e20206f3f443709a9e94
Abel2Code <asalinaswc1@aol.com>
2018-04-21 13:01:42 -0700

    Removed speech recognition from branch

--- SpeechRecognition/ResponseModule.py
+++
@@ -1,8 +0,0 @@
-import pyttsx
-
-# Initialize Text-to-Speech
-engine = pyttsx.init()
-
-def say(text):
-    engine.say(text)
-    engine.runAndWait()
--- SpeechRecognition/test2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from SpeechRecognitionModule import getVoiceInput
-from ResponseModule import say
-
-# Comparign algoritihm can compare length and (first or last letter)
-
-say('Say Something')
-say(getVoiceInput())
